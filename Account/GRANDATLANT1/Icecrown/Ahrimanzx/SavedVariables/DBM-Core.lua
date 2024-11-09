aad83b90f1244e0e71b83c0f348837b9b616afdc
grandatlant <grandatlant@gmail.com>
2024-11-09 06:19:53 +0200

    Midway commit

---
+++ Account/GRANDATLANT1/Icecrown/Ahrimanzx/SavedVariables/DBM-Core.lua
@@ -0,0 +1,5 @@
+
+DBM_UsedProfile = "Default"
+DBM_UseDualProfile = false
+DBM_CharSavedRevision = 20240903092929
+DBM_SavedOptions = nil
---
+++ Account/GRANDATLANT1/Icecrown/Ahrimanzx/SavedVariables/EnergyWatch.lua
@@ -0,0 +1,8 @@
+
+EnergyWatch_Save = {
+	["show"] = 0,
+	["scale"] = 1,
+	["text"] = "&e/&em (&c)",
+	["alpha"] = 1,
+	["status"] = 0,
+}
