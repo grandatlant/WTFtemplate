df26b83355089a849edf5a29b23ce7f257f4bef6
grandatlant <grandatlant@gmail.com>
2024-11-10 08:59:21 +0200

    Add base settings for Engraved

---
+++ Account/BaseAccount/BaseRealm/BaseToon/SavedVariables/Engraved.lua
@@ -0,0 +1,98 @@
+
+Engraved_Settings = {
+	["UnusableOpacity"] = 0.3,
+	["TimerMethod"] = "Fill",
+	["AlmostOpacity"] = 0.7,
+	["InCombatOpacity"] = 1,
+	["RuneColor"] = {
+		{
+			0.8, -- [1]
+			0.1, -- [2]
+			0, -- [3]
+		}, -- [1]
+		{
+			0.3, -- [1]
+			0.6, -- [2]
+			0, -- [3]
+		}, -- [2]
+		{
+			0, -- [1]
+			0.4, -- [2]
+			0.6, -- [3]
+		}, -- [3]
+		{
+			0.6, -- [1]
+			0, -- [2]
+			0.7, -- [3]
+		}, -- [4]
+	},
+	["OutOfCombatOpacity"] = 1,
+	["Locked"] = true,
+	["Position"] = {
+		"CENTER", -- [1]
+		"CENTER", -- [2]
+		0, -- [3]
+		0, -- [4]
+	},
+	["Version"] = "1.5",
+	["RuneSettings"] = {
+		{
+			["Scale"] = 0.3,
+			["Position"] = {
+				"CENTER", -- [1]
+				"CENTER", -- [2]
+				-320, -- [3]
+				-500, -- [4]
+			},
+		}, -- [1]
+		{
+			["Scale"] = 0.3,
+			["Position"] = {
+				"CENTER", -- [1]
+				"CENTER", -- [2]
+				-190, -- [3]
+				-500, -- [4]
+			},
+		}, -- [2]
+		{
+			["Scale"] = 0.3,
+			["Position"] = {
+				"CENTER", -- [1]
+				"CENTER", -- [2]
+				210, -- [3]
+				-500, -- [4]
+			},
+		}, -- [3]
+		{
+			["Scale"] = 0.3,
+			["Position"] = {
+				"CENTER", -- [1]
+				"CENTER", -- [2]
+				350, -- [3]
+				-500, -- [4]
+			},
+		}, -- [4]
+		{
+			["Scale"] = 0.3,
+			["Position"] = {
+				"CENTER", -- [1]
+				"CENTER", -- [2]
+				-50, -- [3]
+				-500, -- [4]
+			},
+		}, -- [5]
+		{
+			["Scale"] = 0.3,
+			["Position"] = {
+				"CENTER", -- [1]
+				"CENTER", -- [2]
+				70, -- [3]
+				-500, -- [4]
+			},
+		}, -- [6]
+	},
+	["Prioritize"] = false,
+	["AlmostTime"] = 1.5,
+	["Theme"] = "Glyph",
+	["AnimateGlow"] = true,
+}
