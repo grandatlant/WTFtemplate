e2cce2da7040bf6656581f4d97ec53ecddf72f89
grandatlant <grandatlant@gmail.com>
2024-11-14 11:18:01 +0200

    Add DrainSoulTimer

---
+++ Account/BaseAccount/BaseRealm/BaseToon/SavedVariables/DrainSoulTimer.lua
@@ -0,0 +1,19 @@
+
+DST_Vars = {
+	["Tick_Raidwarning"] = 1,
+	["Tick_Sound"] = 1,
+	["EW_Raidwarning"] = 1,
+	["EW_Sound"] = 1,
+	["EW_Percentage"] = 25,
+	["EW_Min_HP"] = 180000,
+	["EW_Color"] = {
+		["r"] = 1,
+		["g"] = 0,
+		["b"] = 0.75,
+	},
+	["Tick_Color"] = {
+		["r"] = 0,
+		["g"] = 1,
+		["b"] = 0.75,
+	},
+}
