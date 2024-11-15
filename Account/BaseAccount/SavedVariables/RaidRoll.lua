642afd6d79d893fa799cf3c434dcbe3ec76596c9
grandatlant <grandatlant@gmail.com>
2024-11-15 07:08:31 +0200

    Settings and layout for RaidRoll addon

---
+++ Account/BaseAccount/SavedVariables/RaidRoll.lua
@@ -0,0 +1,34 @@
+
+RaidRoll_DB = {
+	["MIN_GP"] = 1000,
+	["Rank Priority"] = {
+		10, -- [1]
+		9, -- [2]
+		8, -- [3]
+		7, -- [4]
+		6, -- [5]
+		5, -- [6]
+		4, -- [7]
+		3, -- [8]
+		2, -- [9]
+		1, -- [10]
+		0, -- [11]
+		-1, -- [12]
+	},
+	["Scale"] = 100,
+	["BASE_GP"] = 1000,
+	["DECAY_P"] = 0,
+	["EXTRAS_P"] = 0,
+	["EPGP"] = {
+	},
+	["Loot"] = {
+		[0] = {
+			["TOTAL ITEMS"] = 0,
+		},
+		["TOTAL WINDOWS"] = 0,
+		["CURRENT WINDOW"] = 0,
+	},
+	["MIN_EP"] = 1000,
+}
+RaidRoll_DBPC = {
+}
