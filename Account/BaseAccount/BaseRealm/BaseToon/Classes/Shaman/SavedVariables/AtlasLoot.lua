158cc18ba4b2721a260489fae6e63df85105b54b
grandatlant <grandatlant@gmail.com>
2024-11-25 17:08:34 +0200

    Add Classes/* macros-cache.txt and SavedVariables for all WotLK classes

---
+++ Account/BaseAccount/BaseRealm/BaseToon/Classes/Shaman/SavedVariables/AtlasLoot.lua
@@ -0,0 +1,54 @@
+
+AtlasLootCharDB = {
+	["QuickLooks"] = {
+	},
+	["WishList"] = {
+	},
+	["AtlasLootVersion"] = "51104",
+	["AutoQuery"] = false,
+	["SearchResult"] = {
+	},
+}
+AtlasLootFilterDB = {
+	["Relics"] = {
+		["Idol"] = false,
+		["Libram"] = false,
+		["Sigils"] = false,
+		["Totem"] = true,
+	},
+	["WeaponsRanged"] = {
+		["Crossbow"] = false,
+		["Thrown"] = false,
+		["Gun"] = false,
+		["Bow"] = false,
+		["Wand"] = false,
+	},
+	["Armor"] = {
+		["Leather"] = true,
+		["Plate"] = false,
+		["Mail"] = true,
+		["Cloth"] = true,
+	},
+	["WeaponsMeeleTwoHand"] = {
+		["Mace"] = true,
+		["Sword"] = false,
+		["Axe"] = true,
+	},
+	["Other"] = {
+		["Neck"] = true,
+		["Trinket"] = true,
+		["Ring"] = true,
+		["Back"] = true,
+	},
+	["WeaponsMeele"] = {
+		["Axe"] = true,
+		["Shield"] = true,
+		["Held in Off-Hand"] = true,
+		["Staff"] = true,
+		["Dagger"] = true,
+		["Mace"] = true,
+		["Fist Weapon"] = true,
+		["Polearm"] = false,
+		["Sword"] = false,
+	},
+}
