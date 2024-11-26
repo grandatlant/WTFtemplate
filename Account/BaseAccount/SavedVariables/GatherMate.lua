8925e8e8f54cf1a3fdc0b9d6fdede50aa7cec44a
grandatlant <grandatlant@gmail.com>
2024-11-26 10:17:00 +0200

    Add settings for GatherMate

---
+++ Account/BaseAccount/SavedVariables/GatherMate.lua
@@ -0,0 +1,27 @@
+
+GatherMateDB = {
+	["profileKeys"] = {
+	},
+	["profiles"] = {
+		["Default"] = {
+			["show"] = {
+				["Herb Gathering"] = "active",
+				["Mining"] = "active",
+			},
+			["importers"] = {
+				["GatherMate_Data"] = {
+					["Databases"] = {
+						["Fish"] = true,
+						["Treasure"] = true,
+						["Mines"] = true,
+						["Gases"] = true,
+						["Herbs"] = true,
+					},
+					["Style"] = "Overwrite",
+					["lastImport"] = 274,
+				},
+			},
+			["minimapTooltips"] = false,
+		},
+	},
+}
