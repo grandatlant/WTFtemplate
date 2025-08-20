ddb70c8012684fd89bfb65c68dae1467e96ad41c
grandatlant <grandatlant@gmail.com>
2025-08-20 06:15:51 +0300

    Rename DeathKnight -> Deathknight and FloTotemBar settings for Hunter Class

---
+++ Account/BaseAccount/BaseRealm/Classes/Hunter/SavedVariables/FloTotemBar.lua
@@ -0,0 +1,56 @@
+
+FLOTOTEMBAR_OPTIONS = {
+	{
+		["scale"] = 1,
+		["borders"] = true,
+		["barSettings"] = {
+			["TRAP"] = {
+				["color"] = {
+					0.49, -- [1]
+					0.49, -- [2]
+					0, -- [3]
+					0.7, -- [4]
+				},
+				["hiddenSpells"] = {
+				},
+				["position"] = "auto",
+				["buttonsOrder"] = {
+					4, -- [1]
+					1, -- [2]
+					5, -- [3]
+					3, -- [4]
+					2, -- [5]
+					6, -- [6]
+				},
+			},
+		},
+		["barLayout"] = "1row",
+	}, -- [1]
+	{
+		["scale"] = 1,
+		["borders"] = true,
+		["barLayout"] = "1row",
+		["barSettings"] = {
+			["TRAP"] = {
+				["color"] = {
+					0.49, -- [1]
+					0.49, -- [2]
+					0, -- [3]
+					0.7, -- [4]
+				},
+				["hiddenSpells"] = {
+				},
+				["buttonsOrder"] = {
+					2, -- [1]
+					3, -- [2]
+					6, -- [3]
+					4, -- [4]
+					1, -- [5]
+					5, -- [6]
+				},
+				["position"] = "auto",
+			},
+		},
+	}, -- [2]
+	["active"] = 1,
+}
